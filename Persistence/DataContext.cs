63f010b00ce0607b638d4a895418b4a26ab2081d
Costin Somodea <costin.somodea@yahoo.com>
2020-10-20 18:40:33 +0300

    CQRS and MediatR clean architecture

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -7,6 +7,7 @@ namespace Persistence {
         public DataContext (DbContextOptions options) : base (options) { }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities {get; set;}
 
         protected override void OnModelCreating (ModelBuilder builder) {
             builder.Entity<Value> ().HasData (
