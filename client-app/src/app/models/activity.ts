6d42148133537243f5c28c96ae160ef0aae753b1
Costin Somodea <costin.somodea@yahoo.com>
2020-11-06 14:52:18 +0200

    form

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -3,7 +3,29 @@ export interface IActivity {
     title: string;
     description: string;
     category: string;
-    date: string;
+    date: Date; // | null; //Date union null
     city: string;
     venue: string;
+}
+
+export interface IActivityFormValues extends Partial<IActivity> { //this way all of the properties inside IActivityFormValues are optional
+    time?: Date
+}
+
+export class ActivityFormValues implements IActivityFormValues {
+    id?: string = undefined;
+    title: string = "";
+    category: string = "";
+    description: string = "";
+    date?: Date = undefined;
+    time?: Date = undefined;
+    city: string = "";
+    venue: string = "";
+
+    constructor(init? : IActivityFormValues) {
+        if (init && init.date) {
+            init.time = init.date
+        }
+        Object.assign(this, init);
+    }
 }
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,6 +1,8 @@
 import {observable, action, computed, configure, runInAction} from 'mobx';
 import { SyntheticEvent } from 'react';
 import { createContext } from 'react';
+import { toast } from 'react-toastify';
+import { history } from '../..';
 import agent from '../api/agent';
 import { IActivity } from '../models/activity';
 
@@ -20,11 +22,11 @@ export class ActivityStore {
 
     groupActivitiesByDate(activities: IActivity[]) {
         const sortedActivities = activities.sort(
-            (a,b)=> Date.parse(a.date) - Date.parse(b.date)
+            (a,b)=> a.date.getTime() - b.date.getTime()
         )
 
         return Object.entries(sortedActivities.reduce((activities, activity) => {
-            const date = activity.date.split('T')[0];
+            const date = activity.date.toISOString().split('T')[0];
             activities[date] = activities[date] ? [...activities[date], activity] : [activity];
             return activities;
         }, {} as {[key: string]: IActivity[]}));
@@ -36,7 +38,7 @@ export class ActivityStore {
             const activities = await agent.Activities.list();
             runInAction('loading activities', () => {
                 activities.forEach((activity) => {
-                    activity.date = activity.date.split(".")[0];
+                    activity.date = new Date(activity.date);
                     this.activityRegistry.set(activity.id, activity);
                 });
                 this.loadingInitial = false;
@@ -53,14 +55,18 @@ export class ActivityStore {
         let activity = this.getActivity(id);
         if(activity){
             this.activity = activity;
+            return activity;
         } else {
             this.loadingInitial = true;
             try {
                 activity = await agent.Activities.details(id);
                 runInAction('getting activity', () => {
+                    activity.date =  new Date(activity.date);
                     this.activity = activity;
+                    this.activityRegistry.set(activity.id, activity);
                     this.loadingInitial = false;
                 })
+                return activity;
             } catch(error) {
                 runInAction('get activity error', () => {
                     this.loadingInitial = false;
@@ -86,12 +92,13 @@ export class ActivityStore {
                 this.activityRegistry.set(activity.id, activity);
                 this.submitting = false;
             });
-          
+            history.push(`/activities/${activity.id}`)
         } catch(error) {
             runInAction('create activity error', () => { 
                 this.submitting = false;
             });
-            console.log(error);      
+            toast.error('Problem submitting data');
+            console.log(error.response);      
         }
     }
 
@@ -104,11 +111,13 @@ export class ActivityStore {
                 this.activity = activity;
                 this.submitting = false;
             });  
+            history.push(`/activities/${activity.id}`)
         } catch(error) {
             runInAction('edit activity error', () => {
                 this.submitting = false;
             });
-            console.log(error);     
+            toast.error('Problem submitting data');
+            console.log(error.response);     
         }
     }
 
