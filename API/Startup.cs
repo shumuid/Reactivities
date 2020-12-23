6d073944f98fcd625224bd833af5b47666fd59fc
Costin Somodea <costin.somodea@yahoo.com>
2020-12-23 17:40:59 +0200

    private routes and logout on token expiry

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using System.Text;
 using API.Middleware;
@@ -48,7 +49,12 @@ namespace API
              {
                  opt.AddPolicy("CorsPolicy", policy =>
                  {
-                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000").AllowCredentials();
+                     policy
+                     .AllowAnyHeader()
+                     .AllowAnyMethod()
+                     .WithExposedHeaders("WWW-Authenticate")
+                     .WithOrigins("http://localhost:3000")
+                     .AllowCredentials();
                  });
              });
 
@@ -93,7 +99,9 @@ namespace API
                     ValidateIssuerSigningKey = true,
                     IssuerSigningKey = key,
                     ValidateAudience = false,
-                    ValidateIssuer = false
+                    ValidateIssuer = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
                 };
                 opt.Events = new JwtBearerEvents
                 {
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -20,6 +20,7 @@ import { RootStoreContext } from "../stores/rootStore";
 import { LoadingComponent } from "./LoadingComponent";
 import ModalContainer from "../common/modals/ModalContainer";
 import ProfilePage from "../../features/profiles/ProfilePage";
+import PrivateRoute from "./PrivateRoute";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   const rootStore = useContext(RootStoreContext);
@@ -48,15 +49,25 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
               <Switch>
-                <Route exact path='/activities' component={ActivityDashboard} />
-                <Route path='/activities/:id' component={ActivityDetails} />
+                <PrivateRoute
+                  exact
+                  path='/activities'
+                  component={ActivityDashboard}
+                />
+                <PrivateRoute
+                  path='/activities/:id'
+                  component={ActivityDetails}
+                />
                 {/* add a key to rerender the component with a fresh one */}
-                <Route
+                <PrivateRoute
                   key={location.key}
                   path={["/createActivity", "/manage/:id"]}
                   component={ActivityForm}
                 />
-                <Route path='/profile/:username' component={ProfilePage} />
+                <PrivateRoute
+                  path='/profile/:username'
+                  component={ProfilePage}
+                />
                 <Route component={NotFound} />
               </Switch>
             </Container>
